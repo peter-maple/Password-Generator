5bf87f2e8d8efad5e91ae792f07a75e855ce94df
Peter Maple <129986016+peter-maple@users.noreply.github.com>
2024-02-16 17:06:32 -0500

    Updated spellphabet words

--- src/index.ts
+++ src/index.ts
@@ -248,15 +248,15 @@ const placesSpellphabet = new SpellingAlphabet(
         "apple",
         "bagel",
         "cactus",
-        "dragon",
-        "empire",
-        "feisty",
+        "desert",
+        "eagle",
+        "fluffy",
         "goblet",
         "happy",
         "igloo",
         "joker",
         "king",
-        "loudly",
+        "listen",
         "magic",
         "north",
         "omelet",
@@ -266,7 +266,7 @@ const placesSpellphabet = new SpellingAlphabet(
         "swamp",
         "token",
         "unique",
-        "vampire",
+        "vacuum",
         "water",
         "x-ray",
         "yellow",
@@ -736,39 +736,3 @@ window.addEventListener(
     },
     true
 );
-
-//
-//
-//
-
-//
-//
-//
-
-// add ability to provide CSV list for custom phonetic alphabet
-
-// style length [number] input to have separate + - buttons
-
-//  ----- then display the custom spellphabets in that dropdown menu (add name field to custom input to label list in the menu--provide a default incrementing name if none is selected)
-// add ability to delete custom word lists
-// add ability to export custom word lists ----- could just have a popover dialogue that gives a text field populated with the "csv" text
-
-// at narrowness breakpoint, switch settings list from row display to column
-
-// remove alert if no boxes checked. instead implement model like dashlane where you can't uncheck minimum # of boxes. but maybe throw a little error message so it's less confusing.
-
-//
-// || Done:
-//
-
-// figure out how to exclude commonly confused characters
-// color numbers and symbols differently from letters
-
-// a b c d e f g h i j k l m n o p q r s t u v w x y z
-// add phonetic alphabet field
-
-// set it all up so that the chosen basic settings can be shared
-// either using URL modifiers like the old one or with a settings export/import tool. latter is worse for single-action sharing of the tool.
-// either way requires figuring out how to make the settings reference a text area or something...
-
-// add alternate provided spellphabets that users can select from a dropdown menu
